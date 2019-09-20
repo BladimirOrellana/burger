e6359b12504539edd8279e96299feb19cbe27c02
Bladimir Orellana <me@bladimirorellana.com>
2019-09-19 23:08:29 -0500

    Created index file public and app folder

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const handlebars = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
